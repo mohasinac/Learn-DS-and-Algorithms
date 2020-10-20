f7882b2ec9db315695ae93cc82a9121b8aeef91b
mohasinac <mohsin.ahamed@hotmail.com>
2020-10-20 22:11:16 +0530

    insertion sort and merge sorts added

---
+++ js/sorts/insertionSort.js
@@ -0,0 +1,25 @@
+/* Works as if we insert a new card into an already sorted set of cards
+    At each iteration we keep on swapping the position of the new card with the old cards until we find the 
+    correct position of the new card i.e. no other card is smaller than it
+    e.g. cards be 2 4 5 
+     we get new card say 3 
+     we swap 3 with 5 i.e. 2 4 3 5
+     we swap 3 with 4 i.e. 2 3 4 5 
+     we cannot swap any further as 3 is at its correct place so we stop 
+*/
+function InsertionSort(inputArr) {
+    let length = inputArr.length;
+    for (let i = 1; i < length; i++) {
+        let key = inputArr[i];
+        let j = i - 1;
+        while (j >= 0 && inputArr[j] > key) {
+            inputArr[j + 1] = inputArr[j];
+            j = j - 1;
+        }
+        inputArr[j + 1] = key;
+    }
+    return inputArr;
+}
+
+console.log(InsertionSort([3,2,1]))
+exports.InsertionSort;
---
+++ js/sorts/mergeSort.js
@@ -0,0 +1,33 @@
+// compare the arrays item by item and return the concatenated result
+function merge(left, right) {
+    let result = []
+    let leftIndex = 0
+    let rightIndex = 0
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex])
+            leftIndex++
+        } else {
+            result.push(right[rightIndex])
+            rightIndex++
+        }
+    }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+}
+function mSort(array) {
+    if (array.length === 1) {
+        return array                            // return once we hit an array with a single item
+    }
+    const middle = Math.floor(array.length / 2) // get the middle item of the array rounded down
+    const left = array.slice(0, middle)         // items on the left side
+    const right = array.slice(middle)           // items on the right side
+    
+    return merge(
+        mSort(left),
+        mSort(right)
+    )
+}
+
+var testArr = [3, 1, 2];
+
+console.log(mSort(testArr));
