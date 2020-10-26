6a11759fdeb2bec867cd6bd5f50cadfc3ae0e6b9
mohasinac <mohsin.ahamed@hotmail.com>
2020-10-26 21:56:07 +0530

    singly linked list skeleton added

---
+++ js/datastructures/linkedlists/singlylinkedlist.js
@@ -0,0 +1,31 @@
+class SllNode {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    insert(data) {
+    }
+    insertAt(data,position){
+
+    }
+    traverse(){
+
+    }
+    delete(node){
+
+    }
+    deleteAt(position){
+
+    }
+}
+
+let sll = new SinglyLinkedList();
+sll.insert(5);
+
