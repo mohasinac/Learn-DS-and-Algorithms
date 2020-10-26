2aff1bafaab766544612a7ea82144175195d6664
mohasinac <mohsin.ahamed@hotmail.com>
2020-10-26 23:08:09 +0530

    SLL insert delete and traverse done

--- js/datastructures/linkedlists/singlylinkedlist.js
+++ js/datastructures/linkedlists/singlylinkedlist.js
@@ -1,31 +1,102 @@
 class SllNode {
-    constructor(data){
+    constructor(data) {
         this.data = data;
         this.next = null;
     }
 }
 
 class SinglyLinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.size = 0;
     }
     insert(data) {
-    }
-    insertAt(data,position){
 
+        let sllNode = new SllNode(data);
+        if (this.size === 0) {
+            this.head = sllNode;
+        }
+        else {
+            let temp = this.head;
+            while (temp.next) {
+                temp = temp.next;
+            }
+            temp.next = sllNode;
+        }
+        this.size += 1;
     }
-    traverse(){
+    insertAt(data, position) {
+        if (position == 0) {
+            sslNode.next = this.head;
+            this.head = sslNode;
+        }
+        else if (position < this.size) {
+            let temp = this.head;
+            let sslNode = new SllNode(data);
+            let pos = 0;
+            while (temp && pos != position - 1) {
+                temp = temp.next;
+                pos++;
 
+                sslNode.next = temp.next;
+                temp.next = sslNode;
+            }
+        }
+        else {
+            console.log('error out of bounds');
+        }
     }
-    delete(node){
-
+    traverse() {
+        let temp = this.head;
+        let count = 0;
+        while (temp) {
+            console.log(temp.data, count++);
+            temp = temp.next;
+        }
+        console.log('');
     }
-    deleteAt(position){
-
+    delete() {
+        let temp = this.head;
+        while (temp.next.next) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+    deleteAt(position) {
+        let temp = this.head;
+        if (this.size <= 0) {
+            return 'Empty List';
+        }
+        else if (position < this.size) {
+            let pos = 0;
+            while (temp && pos != position - 1) {
+                pos += 1;
+                temp = temp.next;
+            }
+            let result = temp.next;
+            temp.next = temp.next.next;
+            return result;
+        }
+        else {
+            console.log('Invalid Position Provided');
+        }
     }
 }
 
 let sll = new SinglyLinkedList();
+sll.traverse();
 sll.insert(5);
+sll.insert(2);
+sll.insert(2);
+sll.insert(9);
+sll.insert(9);
+sll.insert(6)
+sll.insert(6)
+sll.traverse();
+sll.delete();
+sll.insertAt(1, 2);
+sll.insertAt(0, 0);
+sll.insertAt(0, 2);
+sll.deleteAt(5);
+sll.traverse();
 
