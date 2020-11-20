15cfb38aaf5891c6bd61579229abe5c962a92c99
mohasinac <mohsin.ahamed@hotmail.com>
2020-11-21 04:07:20 +0530

    traversals color encoded done

--- js/datastructures/graphs/traversalsColorCoded.js
+++ js/datastructures/graphs/traversalsColorCoded.js
@@ -26,7 +26,7 @@ function  DFSCCUtil(graph, vertex,visited , time ,order){
     visited[vertex].detected = time;
     visited[vertex].color = 'GRAY';
     for(let i=0;i < graph.vCount;i++){
-        console.log(graph.edges[vertex][i],visited[i].color)
+        //console.log(graph.edges[vertex][i],visited[i].color)
         if(visited[i].color == 'WHITE' && graph.edges[vertex][i] == 1){
             visited[i].parent = vertex;
             time = DFSCCUtil(graph,i,visited,time,order);
@@ -38,8 +38,35 @@ function  DFSCCUtil(graph, vertex,visited , time ,order){
     return time;
 }
 
-function BFSCC(){
-
+function BFSCC(graph,source=0){
+    let visited = new Array(graph.vCount);
+    for(let i=0;i<graph.vCount;i++){
+        visited[i] = {
+            color : 'WHITE',
+            parent : null,
+            distance : Number.POSITIVE_INFINITY
+        }
+    }
+    visited[source].color = 'GRAY';
+    visited[source].distance = 0;
+    visited[source].parent = null;
+    let queue = new CircularQueue(graph.vCount);
+    queue.enqueue(source);
+    while(!queue.isEmpty()){
+        let vertex = queue.dequeue();
+        for(let i=0;i<graph.vCount;i++){
+            //console.log(graph.edges[vertex][i],graph.edges[i][vertex])
+            if(visited[i].color == 'WHITE' && 
+                (graph.edges[vertex][i] == 1 || graph.edges[i][vertex] == 1)){
+                    visited[i].color = 'GRAY';
+                    visited[i].distance = visited[vertex].distance + 1;
+                    visited[i].parent = vertex;
+                    queue.enqueue(i);
+            }
+        }
+        visited[vertex].color = 'BLACK';
+    }
+    return visited;
 }
 
 module.exports = {
@@ -51,8 +78,9 @@ module.exports = {
 let g = new Graph(4);
 //vertices start from 0 to vCount-1
 g.addEdge(0, 1);
-//g.addEdge(1, 2);
+g.addEdge(1, 2);
 g.addEdge(1, 3);
 g.addEdge(2, 3);
 
 console.log(DFSCC(g));
+console.log(BFSCC(g));
